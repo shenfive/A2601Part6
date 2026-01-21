a2a950f98329a7c803efdf8f00eaa66348325d5a
Danny Shen <shenfive@gmail.com>
2026-01-21 13:42:26 +0800

    Initial Commit

---
+++ A2601Part6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2601Part6
+//
+//  Created by Danny Shen on 2026/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
